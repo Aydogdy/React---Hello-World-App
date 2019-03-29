2a662f631f1cb556f598fbd51d9c4e5f41194253
Aydogdy Shahyrov <aydogdy@Aydogdys-MacBook-Pro.local>
2019-03-29 11:42:07 -0500

    added event handler, set state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,57 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1,
+    imageUrl: "https://picsum.photos/200",
+    tags: []
   };
+
+  styles = {
+    counter: {
+      fontSize: 30,
+      fontWeight: "bold",
+      marginLeft: 300
+    }
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  incrementCount = id => {
+    console.log(id);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.incrementCount(1)}
+          className="btn btn-secondary btm-sm"
+        >
+          Increment
+        </button>
+
+        {this.renderTags()}
+      </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+
+    return classes;
   }
 
   formatCount() {
